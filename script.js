468ca66adbc7536a115f4b0aacaecb13538b078a
PawelMroczkowski <197113010+PawelMroczkowski@users.noreply.github.com>
2024-12-17 19:02:52 +0100

    first branch version

--- script.js
+++ script.js
@@ -2,83 +2,18 @@
 const taskInput = document.getElementById("taskInput");
 const taskList = document.getElementById("taskList");
 
+
 function addTask() {
-    const taskText=taskInput.value.trim();
-    if (taskText !== "") {
-        const listItem = document.createElement("li");
-        const paragraphElement = document.createElement("p");
-                //task completion
-            listItem.addEventListener("click" , completeTask);
-             
-        listItem.appendChild(paragraphElement);
-        paragraphElement.textContent = taskText;
-        taskList.appendChild(listItem);
-        taskInput.value = "";
-        
-
-
-        //task delete
-        const deleteButton = document.createElement("button");
-        deleteButton.textContent = "Delete";
-        deleteButton.addEventListener("click",deleteTask);
-        listItem.appendChild(deleteButton);
-    }
-
-    saveTaskToLocalStorage();
-}
-
-
-
-//Task Completion
-function completeTask(event) {
-    const task = event.target;
-    task.classList.toggle("completed");
-   // saveTaskToLocalStorage(); not saving properties
-}
-
-//Task Deletion
-function deleteTask(event) {
-    const task = event.target.parentElement;
-    taskList.removeChild(task);
-    saveTaskToLocalStorage();
-}
-
-//Save Tasks to local storage
-function saveTaskToLocalStorage() {
-    const tasks = [];
-    const taskItems = taskList.getElementsByTagName("p");
-
-    for (let i=0; i < taskItems.length; i++) {
-        tasks.push(taskItems[i].textContent);
-    }
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-}
-
-function loadTasksFromLocalStorage() {
-    const tasks = JSON.parse(localStorage.getItem("tasks"));
-    if (tasks) {
-        tasks.forEach(taskText => {
-            const listItem = document.createElement("li");
-            const paragraphElement = document.createElement("p");
-            listItem.appendChild(paragraphElement);
-            paragraphElement.textContent = taskText; // listItem.textContent = taskText;
-            taskList.appendChild(listItem);
-            listItem.addEventListener("click", completeTask);
-
-            const deleteButton = document.createElement("button");
-            deleteButton.textContent = "Delete";
-            deleteButton.addEventListener("click", deleteTask);
-            listItem.appendChild(deleteButton);
-        })
-
+    taskText = taskInput;
+    if (taskText !== "")   
+    {
+        let task = new Task(taskText)
+        alert(task.taskText.value)
     }
 }
 
-function clearLocalStorage()    {
-    localStorage.clear();
-    location.reload();
+
+function Task(taskText) {
+    this.taskText = taskText;
 }
 
-
-loadTasksFromLocalStorage();
