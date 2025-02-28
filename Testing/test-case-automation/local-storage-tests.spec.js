f5c7655e5231954e3a38339960a2606ce15ddf6d
PawelMroczkowski <197113010+PawelMroczkowski@users.noreply.github.com>
2025-02-28 20:23:16 +0100

    Testing: local storage tests: added 5 more steps

--- Testing/test-case-automation/local-storage-tests.spec.js
+++ Testing/test-case-automation/local-storage-tests.spec.js
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, mergeExpects } from "@playwright/test";
 
 test.beforeEach(async ({ page }) => {
   await page.goto("./");
@@ -28,11 +28,13 @@ test("ToDo_015 Local storage checking.", async ({ page }) => {
   const taskInputText = page.getByRole("textbox", {
     name: "Add a new task",
   });
-  const listItem = page.getByRole("listitem");
+
   const addTaskButton = page.getByRole("button", { name: "Add Task" });
 
   await test.step("Check if initial number of tasks is correct", async () => {
-    await loadLocStorage();
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
     //check if 6 tasks are in array
     await expect(tasks.length, "Check if 6 tasks are added").toEqual(6);
   });
@@ -44,7 +46,9 @@ test("ToDo_015 Local storage checking.", async ({ page }) => {
     await taskInputText.fill("Edit at least 2 tasks.");
     await addTaskButton.click();
     //load storage again after adding task
-    await loadLocStorage();
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
     //check if changes are correct
     await expect(
       tasks.length,
@@ -56,25 +60,160 @@ test("ToDo_015 Local storage checking.", async ({ page }) => {
     ).toEqual("Edit at least 2 tasks.");
   });
 
-  await test.step("Edit", async () => {});
-
-  await test.step("Sort A-Z", async () => {});
-
-  await test.step("Toggle completed", async () => {});
-
-  await test.step("Sort Z-A", async () => {});
-
-  await test.step("Delete", async () => {});
-
-  await test.step("Toggle not completed", async () => {});
-
-  await test.step("Clear local storage", async () => {});
-
-  //functions
-
-  async function loadLocStorage() {
+  await test.step("Edit", async () => {
+    let tasks;
+    const listItem = page.getByRole("listitem");
+    const editAtLeast2Tasks = { has: page.getByText("Edit at least 2 tasks.") };
+    //start editing task
+    await listItem.filter(editAtLeast2Tasks).getByText("Edit:").click();
+    let editTextClassBoxValue = await listItem
+      .filter(editAtLeast2Tasks)
+      .locator(".editText");
+    editTextClassBoxValue.click();
+    editTextClassBoxValue.press("ControlOrMeta+a");
+    editTextClassBoxValue.fill("Edit more than 3 tasks.");
+    await listItem
+      .filter(editAtLeast2Tasks)
+      .getByRole("button", { name: "Apply" })
+      .click();
+    //checking if edited task is visible
+    await expect(
+      listItem.filter({ has: page.getByText("Edit more than 3 tasks.") })
+    ).toBeVisible();
+    //checking local storage
     tasks = await page.evaluate(() =>
       JSON.parse(localStorage.getItem("tasks"))
     );
-  }
+    await expect(
+      tasks.filter((task) => task.taskText == "Edit more than 3 tasks.")
+    ).toBeTruthy();
+  });
+
+  await test.step("Sort A-Z", async () => {
+    let i = 0;
+    let sorted_aToz_Tasks = [
+      "Add some tasks to list.",
+      "Check some tasks as completed.",
+      "Delete one task.",
+      "Edit more than 3 tasks.",
+      "Prepare more tests.",
+      "Sort tasks.",
+      "Write To-Do list.",
+    ];
+    await page.getByText("Sort A to Z").click();
+    await expect(
+      page.getByRole("heading", { name: "To-Do List" })
+    ).toBeVisible();
+
+    //loading local storage
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
+
+    for (i = 0; i < 7; i++) {
+      await expect(tasks[i].taskText, "Checking A-Z order").toEqual(
+        sorted_aToz_Tasks[i]
+      );
+    }
+  });
+
+  await test.step("Toggle completed", async () => {
+    const listItem = page.getByRole("listitem");
+    const checkbox = listItem.getByRole("checkbox");
+    //wait for visible element
+    await expect(
+      page.getByRole("heading", { name: "To-Do List" })
+    ).toBeVisible();
+    //load tasks
+    let tasks;
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
+    await checkbox.first().check();
+    await checkbox.nth(3).check();
+    await checkbox.last().check();
+    //load storage
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
+    await expect(
+      tasks[0].completed,
+      "first task completed status"
+    ).toBeTruthy();
+    await expect(
+      tasks[3].completed,
+      "middle task completed status"
+    ).toBeTruthy();
+    await expect(tasks[6].completed, "last task completed status").toBeTruthy();
+  });
+
+  await test.step("Sort Z-A", async () => {
+    let i = 0;
+    let sorted_zToa_Tasks = [
+      "Write To-Do list.",
+      "Sort tasks.",
+      "Prepare more tests.",
+      "Edit more than 3 tasks.",
+      "Delete one task.",
+      "Check some tasks as completed.",
+      "Add some tasks to list.",
+    ];
+    await page.getByText("Sort Z to A").click();
+    await expect(
+      page.getByRole("heading", { name: "To-Do List" })
+    ).toBeVisible();
+
+    //loading local storage
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
+
+    for (i = 0; i < 7; i++) {
+      await expect(tasks[i].taskText, "Checking Z-A order").toEqual(
+        sorted_zToa_Tasks[i]
+      );
+    }
+  });
+
+  await test.step("Delete", async () => {
+    //define elements
+    const listItem = page.getByRole("listitem");
+    const deleteButtonFirst = listItem
+      .first()
+      .getByRole("button", { name: "Delete" });
+    const deleteButtonLast = listItem
+      .last()
+      .getByRole("button", { name: "Delete" });
+    //first and last task as variable
+    //load storage
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
+    let firstTask = tasks[0];
+    let lastTask = tasks[tasks.length - 1];
+
+    //checking if less items visible
+    await expect(listItem, "7 list items visible").toHaveCount(7);
+    await deleteButtonFirst.click();
+    await deleteButtonLast.click();
+    await expect(listItem, "5 list items visible").toHaveCount(5);
+    //load storage
+    tasks = await page.evaluate(() =>
+      JSON.parse(localStorage.getItem("tasks"))
+    );
+    //check if first and last tasks are not present in array from local storage
+    await expect(tasks, "not contain first task").not.toContain(firstTask);
+    await expect(tasks, "not contain last task").not.toContain(lastTask);
+  });
+
+  await test.step("Toggle not completed", async () => {
+    // const listItem = page.getByRole("listitem");
+    // const checkbox = listItem.getByRole("checkbox");
+    // //wait for visible element
+    // await expect(
+    //   page.getByRole("heading", { name: "To-Do List" })
+    // ).toBeVisible();
+  });
+
+  await test.step("Clear local storage", async () => {});
 });
