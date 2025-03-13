67d4a1641fa28858a2a8743a4e377058a85133b7
PawelMroczkowski <197113010+PawelMroczkowski@users.noreply.github.com>
2025-03-13 16:59:20 +0100

    test: added locators for app elements in separate file; changed add-task tests respectively (#12)

--- Testing/test-case-automation/add-task.spec.js
+++ Testing/test-case-automation/add-task.spec.js
@@ -1,97 +1,72 @@
 import { test, expect } from "@playwright/test";
-import { assert } from "console";
+import {
+  addNewTaskTextBox,
+  addNewTaskButton,
+  checkbox,
+  editButton,
+  deleteButton,
+  placeholderText,
+  paragraph,
+} from "../utilities/todo_locators";
 
 test.beforeEach(async ({ page }) => {
-  await page.goto('./');
+  await page.goto("./");
 });
 
 test("ToDo_001 Add task: correct.", async ({ page }) => {
-  await page.getByRole("textbox", { name: "Add a new task" }).click();
-  await page
-    .getByRole("textbox", { name: "Add a new task" })
-    .fill("First task to test.");
-  await page.getByRole("button", { name: "Add Task" }).click();
-  await expect(page.getByRole("paragraph"), "Task added").toContainText(
+  await addNewTaskTextBox(page).fill("First task to test.");
+  await addNewTaskButton(page).click();
+  await expect(paragraph(page), "Task added").toContainText(
     "First task to test."
   );
+  await expect(editButton(page), "Edit button visible").toBeVisible();
+  await expect(deleteButton(page), "Delete button visible").toBeVisible();
+  await expect(checkbox(page), "Checkbox visible").toBeVisible();
+  await expect(addNewTaskTextBox(page)).toBeVisible();
   await expect(
-    page.getByRole("button", { name: "Edit:" }),
-    "Edit button visible"
-  ).toBeVisible();
-  await expect(
-    page.getByRole("button", { name: "Delete" }),
-    "Delete button visible"
-  ).toBeVisible();
-  await expect(page.getByRole("checkbox"), "Checkbox visible").toBeVisible();
-  const placeholderText = await page
-    .getByRole("textbox", { name: "Add a new task" })
-    .getAttribute("placeholder");
-  expect(placeholderText, 'Placeholder text default value').toEqual("Add a new task");
+    await placeholderText(page),
+    "Placeholder text default value"
+  ).toEqual("Add a new task");
 });
 
 test("ToDo_002 Add task: empty.", async ({ page }) => {
-  await page.getByRole("button", { name: "Add Task" }).click();
+  await addNewTaskButton(page).click();
   await expect
-    .soft(
-      page.getByRole("button", { name: "Edit:" }),
-      "Edit button not visible"
-    )
+    .soft(editButton(page), "ERROR EXPECTED Edit button not visible")
     .not.toBeVisible();
   await expect
-    .soft(
-      page.getByRole("button", { name: "Delete" }),
-      "Delete button not visible"
-    )
+    .soft(deleteButton(page), "ERROR EXPECTED Delete button not visible")
     .not.toBeVisible();
   await expect
-    .soft(page.getByRole("checkbox"), "Checkbox not visible")
+    .soft(checkbox(page), "ERROR EXPECTED Checkbox not visible")
     .not.toBeVisible();
+  await expect.soft(paragraph(page), "Paragraph not visible").not.toBeVisible();
 });
 
 test("ToDo_003 Add task: space filled task.", async ({ page }) => {
-  await page.getByRole("textbox", { name: "Add a new task" }).click();
-  await page
-    .getByRole("textbox", { name: "Add a new task" })
-    .fill("            ");
-  await page.getByRole("button", { name: "Add Task" }).click();
-  await expect(page.getByRole("paragraph"), '"Space" text').toContainText(
-    "            "
-  );
+  await addNewTaskTextBox(page).fill("            ");
+  await addNewTaskButton(page).click();
+  await expect(paragraph(page), '"Space" text').toContainText("            ");
+  await expect(editButton(page), "Edit button visible").toBeVisible();
+  await expect(deleteButton(page), "Delete button visible").toBeVisible();
+  await expect(checkbox(page), "Checkbox visible").toBeVisible();
   await expect(
-    page.getByRole("button", { name: "Edit:" }),
-    "Edit button visible"
-  ).toBeVisible();
-  await expect(
-    page.getByRole("button", { name: "Delete" }),
-    "Delete button visible"
-  ).toBeVisible();
-  await expect(page.getByRole("checkbox"), "Checkbox visible").toBeVisible();
-  const placeholderText = await page
-    .getByRole("textbox", { name: "Add a new task" })
-    .getAttribute("placeholder");
-  expect(placeholderText, 'Placeholder text default value').toEqual("Add a new task");
+    await placeholderText(page),
+    "Placeholder text default value"
+  ).toEqual("Add a new task");
 });
 
 test("ToDo_004 Add task: numbers and specials.", async ({ page }) => {
-  await page.getByRole("textbox", { name: "Add a new task" }).click();
-  await page
-    .getByRole("textbox", { name: "Add a new task" })
-    .fill("12345^&*()");
-  await page.getByRole("button", { name: "Add Task" }).click();
-  await expect(page.getByRole("paragraph"), "Special signs task").toContainText(
+  await addNewTaskTextBox(page).fill("12345^&*()");
+  await addNewTaskButton(page).click();
+  await expect(paragraph(page), "Special signs task").toContainText(
     "12345^&*()"
   );
+  await expect(editButton(page), "Edit button visible").toBeVisible();
+  await expect(deleteButton(page), "Delete button visible").toBeVisible();
+  await expect(checkbox(page), "Checkbox visible").toBeVisible();
   await expect(
-    page.getByRole("button", { name: "Edit:" }),
-    "Edit button visible"
-  ).toBeVisible();
-  await expect(
-    page.getByRole("button", { name: "Delete" }),
-    "Delete button visible"
-  ).toBeVisible();
-  await expect(page.getByRole("checkbox"), "Checkbox visible").toBeVisible();
-  const placeholderText = await page
-    .getByRole("textbox", { name: "Add a new task" })
-    .getAttribute("placeholder");
-  expect(placeholderText, 'Placeholder text default value').toEqual("Add a new task");
+    await placeholderText(page),
+    "Placeholder text default value"
+  ).toEqual("Add a new task");
 });
